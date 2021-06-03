2302a8da97996b18ca74bcc82047db1b2e9542c6
Kuba Orlik <kontakt@kuba-orlik.name>
2021-06-03 09:51:10 +0200

    Try using html as log output format

--- jenkins.start.sh
+++ jenkins.start.sh
@@ -23,5 +23,5 @@ docker-compose run --user="$UID"\
 			   -p ${SEALIOUS_PORT}:${SEALIOUS_PORT} \
 			   -d \
 			   test \
-			   /bin/sh -c "node . > log.txt 2>&1" \
+			   /bin/sh -c "node . 2>&1 | ansi2html > log.html" \
 	&& echo "App started on $SEALIOUS_PORT"
