390cffbf3c4406ed154fa346855ba038dbd74c10
Kuba Orlik <kontakt@kuba-orlik.name>
2024-02-13 17:51:34 +0100

    Update the deps to reap benefits from the patches

--- src/back/index.ts
+++ src/back/index.ts
@@ -1,28 +1,30 @@
 import kill from "kill-port";
 import _locreq from "locreq";
 import TheApp from "./app.js";
-import { SEALIOUS_SANITY } from "./config.js";
+import { PORT, SEALIOUS_SANITY } from "./config.js";
 import { mainRouter } from "./routes/index.js";
 import { module_dirname } from "./util.js";
 const locreq = _locreq(module_dirname(import.meta.url));
 
 const app = new TheApp();
 
-kill(app.config["www-server"].port)
-	.then(() => app.start())
-	.then(async () => {
+(async function () {
+	await kill(PORT);
+	await kill(PORT);
+
+	try {
+		app.HTTPServer.addStaticRoute("/", locreq.resolve("public"));
+		await app.start();
 		if (SEALIOUS_SANITY) {
 			console.error("Exiting with error code 0");
 			process.exit(0);
 		}
 		mainRouter(app.HTTPServer.router);
-	})
-	.catch((error) => {
+	} catch (error) {
 		console.error(error);
 		if (SEALIOUS_SANITY) {
 			console.error("EXITING WITH STATUS 1");
 			process.exit(1);
 		}
-	});
-
-app.HTTPServer.addStaticRoute("/", locreq.resolve("public"));
+	}
+})();
