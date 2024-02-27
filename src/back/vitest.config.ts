eff47b3b85a1f808b1daf5f79f9d1ac3bd914bdb
Kuba Orlik <kontakt@kuba-orlik.name>
2024-02-27 13:20:37 +0100

    Fix tsx tests

--- src/back/vitest.config.ts
+++ src/back/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 const exclude = ["docker_node_modules", "node_modules", "dist", "lib", "webhint"];
 
+import AutoImport from "unplugin-auto-import/vite";
+
 export default defineConfig({
 	test: {
 		testTimeout: 10000,
@@ -15,4 +17,11 @@ export default defineConfig({
 			include: ["src/**", "test?(-*).?(c|m)[jt]s?(x)"],
 		},
 	},
+	plugins: [
+		AutoImport({
+			include: [/\.tsx?$/],
+			imports: [{ tempstream: [["TempstreamJSX", "React"]] }],
+			dts: false,
+		}),
+	],
 });
