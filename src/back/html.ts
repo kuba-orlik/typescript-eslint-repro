6a154558e1395bb377f002777f9f72baa7fcd61b
Kuba Orlik <kontakt@kuba-orlik.name>
2024-03-17 13:50:50 +0100

    Update the responsive metatag

--- src/back/html.ts
+++ src/back/html.ts
@@ -10,7 +10,7 @@ export const defaultHead = (
 	title: string,
 	options: HTMLOptions
 ) => /* HTML */ `<title>${title} · ${ctx.$app.manifest.name}</title>
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<script async src="/dist/bundle.js"></script>
 	<link
 		href="/dist/main.css${options.autoRefreshCSS
